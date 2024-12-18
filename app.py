317469465a4de35fbf09eb23ff0a07c76bffb1d7
codewithmissjo <codewithmissjo+jw1@gmail.com>
2024-12-17 18:10:56 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, make_response, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# insert route and function here
+
+if __name__ == '__main__':
+  app.run()
